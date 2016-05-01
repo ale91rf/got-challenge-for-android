48582f4b9f220934ebad39ca741daca9b84f5c46
Ale Ramírez Fernández <alejandro@MacBook-Pro-de-Ale.local>
2016-05-01 21:57:07 +0200

    creating an architecture

---
+++ app/src/main/java/es/npatarino/android/gotchallenge/ui/activity/HomeActivity.java
@@ -0,0 +1,52 @@
+package es.npatarino.android.gotchallenge.ui.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import es.npatarino.android.gotchallenge.R;
+import es.npatarino.android.gotchallenge.ui.adapter.SectionsPagerAdapter;
+
+public class HomeActivity extends AppCompatActivity {
+
+    SectionsPagerAdapter spa;
+    ViewPager vp;
+    Toolbar toolbar;
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setSpa(new SectionsPagerAdapter(getSupportFragmentManager()));
+
+        setVp((ViewPager) findViewById(R.id.container));
+        getVp().setAdapter(getSpa());
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(getVp());
+    }
+
+    public SectionsPagerAdapter getSpa() {
+        return spa;
+    }
+
+    public void setSpa(SectionsPagerAdapter spa) {
+        this.spa = spa;
+    }
+
+    public ViewPager getVp() {
+        return vp;
+    }
+
+    public void setVp(ViewPager vp) {
+        this.vp = vp;
+    }
+
+
+}
---
+++ app/src/main/java/es/npatarino/android/gotchallenge/ui/adapter/GoTAdapter.java
@@ -0,0 +1,105 @@
+package es.npatarino.android.gotchallenge.ui.adapter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import es.npatarino.android.gotchallenge.R;
+import es.npatarino.android.gotchallenge.model.GoTCharacter;
+import es.npatarino.android.gotchallenge.ui.activity.DetailActivity;
+
+/**
+ * Created by alejandro on 1/5/16.
+ */
+public class GoTAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final List<GoTCharacter> gcs;
+    private Activity a;
+
+    public GoTAdapter(Activity activity) {
+        this.gcs = new ArrayList<>();
+        a = activity;
+    }
+
+    public void addAll(Collection<GoTCharacter> collection) {
+        for (int i = 0; i < collection.size(); i++) {
+            gcs.add((GoTCharacter) collection.toArray()[i]);
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new GotCharacterViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.got_character_row, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        GotCharacterViewHolder gotCharacterViewHolder = (GotCharacterViewHolder) holder;
+        gotCharacterViewHolder.render(gcs.get(position));
+        ((GotCharacterViewHolder) holder).imp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Intent intent = new Intent(((GotCharacterViewHolder) holder).itemView.getContext(), DetailActivity.class);
+                intent.putExtra("description", gcs.get(position).mDescription);
+                intent.putExtra("name", gcs.get(position).mName);
+                intent.putExtra("imageUrl", gcs.get(position).mImageUrl);
+                ((GotCharacterViewHolder) holder).itemView.getContext().startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return gcs.size();
+    }
+
+    public class GotCharacterViewHolder extends RecyclerView.ViewHolder {
+
+        private static final String TAG = "GotCharacterViewHolder";
+        ImageView imp;
+        TextView tvn;
+
+        public GotCharacterViewHolder(View itemView) {
+            super(itemView);
+            imp = (ImageView) itemView.findViewById(R.id.ivBackground);
+            tvn = (TextView) itemView.findViewById(R.id.tv_name);
+        }
+
+        public void render(final GoTCharacter goTCharacter) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    URL url = null;
+                    try {
+                        url = new URL(goTCharacter.mImageUrl);
+                        final Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                        a.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                imp.setImageBitmap(bmp);
+                                tvn.setText(goTCharacter.mName);
+                            }
+                        });
+                    } catch (IOException e) {
+                        Log.e(TAG, e.getLocalizedMessage());
+                    }
+                }
+            }).start();
+        }
+    }
+
+}
